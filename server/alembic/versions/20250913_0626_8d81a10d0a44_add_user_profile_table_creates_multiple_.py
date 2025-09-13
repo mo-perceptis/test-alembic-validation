672cbe7725d5a3bcdaafa14d95d5effa623b2078
Test User <test@example.com>
2025-09-13 06:28:28 -0700

    Add migration that creates multiple heads - should fail PR validation

---
+++ server/alembic/versions/20250913_0626_8d81a10d0a44_add_user_profile_table_creates_multiple_.py
@@ -0,0 +1,24 @@
+"""Add user profile table - creates multiple heads
+
+Revision ID: 8d81a10d0a44
+Revises: 3cef0dfa8e18
+Create Date: 2025-09-13 06:26:54.055258
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8d81a10d0a44'
+down_revision = '2036c8e50052'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
